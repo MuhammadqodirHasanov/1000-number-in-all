48c8da1b462b99f7305b2274e280820d578e0b9a
Muhammadqodir_Developer <108737014+MuhammadqodirHasanov@users.noreply.github.com>
2022-10-20 20:12:16 +0300

    for practice in 1000 numbers

---
+++ app.js
@@ -0,0 +1,35 @@
+// 1000 son icida,
+// toqlarini, juftlarini, ulani yigindisi 
+// 3 bolinadiganlani
+// topish!
+
+
+// juft sonlar
+// for(let i = 0; i <= 1000; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// toq sonlar
+// for(let i = 0; i <= 1000; i++) {
+//     if (i % 2 > 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// 3 ga bolinadiganlar
+// for(let i = 0; i <= 1000; i++){
+//     if(i % 3 == 0){
+//         console.log(i);
+//     }
+// }
+
+// yig'indisi
+// let sum = 0
+// for(let i = 0; i <= 1000; i++){
+//     sum += i
+// }
+// console.log(sum);
